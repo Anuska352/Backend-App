acd74443b2a7146a04295f98a8f8607b5b99ca25
Anuska352 <anuskachattopadhyay561@gmail.com>
2023-06-21 21:27:13 +0530

    added edit and edited index

--- index.js
+++ index.js
@@ -16,16 +16,38 @@ async function main() {
 
 app.set("views", path.join(__dirname, "views" ));
 app.set("view engine","ejs");
+app.use(express.urlencoded({extended: true}));
 
 app.get("/products",async (req,res) => {
   const products = await Product.find({})
     res.render('products/index', {products})
 })
 
+app.get("/products/new", (req,res) => {
+  res.render("products/new")
+})
+
+app.post("/products", async (req,res) => {
+  const newProduct = new Product(req.body);
+  await newProduct.save();
+  res.redirect(`/products/${newProduct._id}`);
+
+})
+
 app.get('/products/:id' , async (req,res) => {
-    const { id } = req.params
+    const { id } = req.params;
     const product = await Product.findById(id)
     res.render('products/show.ejs' , { product })
+})
+
+app.get("/products/:id/edit", async (req,res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/edit", {product})
+})
+
+app.put("/products/:id", async(req,res) => {
+  
 })
 
 app.listen(3000, () => {
