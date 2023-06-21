7f65741a10f505e4882277f8f9ced22100452b07
Anuska352 <anuskachattopadhyay561@gmail.com>
2023-06-22 00:43:15 +0530

    CRUD

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const path = require("path");
+const methodOverride = require("method-override");
+
+
 
 const app = express();
 
@@ -16,7 +19,12 @@ async function main() {
 
 app.set("views", path.join(__dirname, "views" ));
 app.set("view engine","ejs");
+app.use(methodOverride("_method"));
 app.use(express.urlencoded({extended: true}));
+
+const categories =["fruit", "vegetables", "dairy", "fungi", "baked goods"];
+
+
 
 app.get("/products",async (req,res) => {
   const products = await Product.find({})
@@ -24,7 +32,7 @@ app.get("/products",async (req,res) => {
 })
 
 app.get("/products/new", (req,res) => {
-  res.render("products/new")
+  res.render("products/new", { categories })
 })
 
 app.post("/products", async (req,res) => {
@@ -43,11 +51,13 @@ app.get('/products/:id' , async (req,res) => {
 app.get("/products/:id/edit", async (req,res) => {
   const { id } = req.params;
   const product = await Product.findById(id);
-  res.render("products/edit", {product})
+  res.render("products/edit", {product, categories})
 })
 
 app.put("/products/:id", async(req,res) => {
-  
+  const { id } = req.params;
+  const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true});
+  res.redirect(`/products/${product._id}`);
 })
 
 app.listen(3000, () => {
